39e2c05af8de3d379991ea50ed96ba541df724e6
LeeLu1k <119786889+LeeLu1k@users.noreply.github.com>
2025-11-06 21:06:30 +0500

    Update script.js

--- webapp/script.js
+++ webapp/script.js
@@ -1,54 +1,30 @@
-let playerHealth = 100;
-let botHealth = 100;
+let playerHP = 100;
+let botHP = 100;
 
-function playerMove(action) {
-    let log = document.getElementById("log");
-    let botAction = ["attack", "defense", "heal"][Math.floor(Math.random() * 3)];
+function attack() {
+    const playerDamage = Math.floor(Math.random() * 20) + 5;
+    const botDamage = Math.floor(Math.random() * 15) + 5;
+    botHP -= playerDamage;
+    playerHP -= botDamage;
+    updateStatus();
+}
 
-    // --- Действие игрока ---
-    if (action === "attack") {
-        if (botAction !== "defense") {
-            botHealth -= 20;
-            log.innerHTML += `<p>Вы атаковали, бот получил 20 урона!</p>`;
-        } else {
-            log.innerHTML += `<p>Вы атаковали, но бот защитился!</p>`;
-        }
-    } else if (action === "defense") {
-        log.innerHTML += `<p>Вы защитились!</p>`;
-    } else if (action === "heal") {
-        playerHealth += 15;
-        if (playerHealth > 100) playerHealth = 100;
-        log.innerHTML += `<p>Вы исцелились на 15 HP!</p>`;
-    }
+function heal() {
+    const healAmount = Math.floor(Math.random() * 20) + 5;
+    playerHP += healAmount;
+    const botDamage = Math.floor(Math.random() * 15) + 5;
+    playerHP -= botDamage;
+    updateStatus();
+}
 
-    // --- Действие бота ---
-    if (botAction === "attack") {
-        if (action !== "defense") {
-            playerHealth -= 20;
-            log.innerHTML += `<p>Бот атаковал, вы получили 20 урона!</p>`;
-        } else {
-            log.innerHTML += `<p>Бот атаковал, но вы защитились!</p>`;
-        }
-    } else if (botAction === "heal") {
-        botHealth += 15;
-        if (botHealth > 100) botHealth = 100;
-        log.innerHTML += `<p>Бот исцелился на 15 HP!</p>`;
+function updateStatus() {
+    if(playerHP <= 0 && botHP <= 0){
+        document.getElementById("status").innerText = "Ничья!";
+    } else if(playerHP <= 0){
+        document.getElementById("status").innerText = "Вы проиграли!";
+    } else if(botHP <= 0){
+        document.getElementById("status").innerText = "Вы победили!";
     } else {
-        log.innerHTML += `<p>Бот защитился!</p>`;
-    }
-
-    document.getElementById("playerHealth").innerText = playerHealth;
-    document.getElementById("botHealth").innerText = botHealth;
-
-    // --- Проверка конца игры ---
-    if (playerHealth <= 0 || botHealth <= 0) {
-        if (playerHealth <= 0 && botHealth <= 0) alert("Ничья!");
-        else if (playerHealth <= 0) alert("Вы проиграли!");
-        else alert("Вы выиграли!");
-        playerHealth = 100;
-        botHealth = 100;
-        log.innerHTML = "";
-        document.getElementById("playerHealth").innerText = playerHealth;
-        document.getElementById("botHealth").innerText = botHealth;
+        document.getElementById("status").innerText = `Ваше HP: ${playerHP} | Бот HP: ${botHP}`;
     }
 }
